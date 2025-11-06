62988cb34ac4b772bdda4fee9dcd9322b3662d1e
tamer <tamers2004@gmail.com>
2025-11-06 21:27:24 +0200

    add end points (day 2)

--- server/main.js
+++ server/main.js
@@ -2,17 +2,46 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+let counter = 0
+const posts = []
+
 app.use(express.json())
 
+app.post('/register', (req,res) => {
+    
+    console.log(req.body)
 
-
-app.get('/', (req, res) => {
-    console.log('Hello Tamer!')
-    res.send({
-        message: 'Hello Tamer!',
-        age:22
+    if (!req.body) {
+        return res.send({
+            status : 'not ok',
+            message : 'body is empty'
+            
+        })
+        
+    }
+    
+        
+    posts.push(req.body)
+    
+    return res.send({
+        status : 'ok'
     })
 })
+
+app.get('/get_all', (req,res) => {
+
+    return res.send({
+        posts
+    })
+})
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
