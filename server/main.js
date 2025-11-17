f5cb32865b019effd3fe625c34c44067c08e3c97
tamer <tamers2004@gmail.com>
2025-11-17 20:40:59 +0200

    remove end points

--- server/main.js
+++ server/main.js
@@ -2,38 +2,10 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-let counter = 0
-const posts = []
+
 
 app.use(express.json())
 
-app.post('/register', (req,res) => {
-    
-    console.log(req.body)
-
-    if (!req.body) {
-        return res.send({
-            status : 'not ok',
-            message : 'body is empty'
-            
-        })
-        
-    }
-    
-        
-    posts.push(req.body)
-    
-    return res.send({
-        status : 'ok'
-    })
-})
-
-app.get('/get_all', (req,res) => {
-
-    return res.send({
-        posts
-    })
-})
 
 
 
